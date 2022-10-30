932b008aaf3e23e6e238ead68eee3f7965902ae6
Manav Naharwal <manavnaharwalp@gmail.com>
2022-10-31 02:45:13 +0530

    Fix small ui issues

--- components/progress/Bar.js
+++ components/progress/Bar.js
@@ -1,7 +1,7 @@
 export const Bar = ({ animationDuration, progress }) => {
   return (
     <div
-      className="bg-white h-1 w-full left-0 top-0 fixed z-50 rounded-sm"
+      className="bg-white h-[0.2rem] w-full left-0 top-0 fixed z-50 rounded-sm"
       style={{
         marginLeft: `${(-1 + progress) * 100}%`,
         transition: `marginLeft ${animationDuration}ms linear`,
